dcafad05209cc406ece399d4508d989a00b0804f
Anigala Omeshamisu <60349557+Omesham@users.noreply.github.com>
2025-07-21 17:51:17 -0500

    Update run_preprocess.sh

--- run_preprocess.sh
+++ run_preprocess.sh
@@ -1,1 +1,8 @@
 
+#!/usr/bin/env bash
+# Make executable:  chmod +x run_preprocess.sh
+
+python preprocess_dataset.py \
+  --dataset /path/to/your/dataset \
+  --out     embeddings \
+  --device  cuda
