d12f0c9865af8e01a4cbb530085f0189af73592a
Anigala Omeshamisu <60349557+Omesham@users.noreply.github.com>
2025-08-01 19:11:02 -0500

    Update run_preprocess.sh

--- run_preprocess.sh
+++ run_preprocess.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 # Make executable:  chmod +x run_preprocess.sh
 
@@ -6,6 +5,6 @@ python preprocess_dataset.py \
     --dataset /data/my_dataset \
     --out embeddings \
     --device cuda \
+    --max-items 10 \
     --use_clap \
     --use_speecht5
-
